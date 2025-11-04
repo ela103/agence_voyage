cec755e230e59287ef37975c932a606c0ebaa13d
issra103 <chikhaouiissra525@gmail.com>
2025-11-04 21:02:47 +0100

    ajout des classes du package service

---
+++ src/main/java/com/ela/service/ClientService.java
@@ -0,0 +1,33 @@
+package com.ela.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ela.projet.model.Client;
+import com.ela.projet.repository.ClientRepository;
+
+@Service
+public class ClientService {
+//service pour la gestion des clients
+    @Autowired
+    private ClientRepository clientRepository;
+
+    public Client ajouterClient(Client client) {
+        return clientRepository.save(client);
+    }
+
+    public List<Client> listerClients() {
+        return clientRepository.findAll();
+    }
+
+    public Client trouverClientParId(Long id) {
+        return clientRepository.findById(id)
+            .orElseThrow(() -> new RuntimeException("Client non trouvé"));
+    }
+
+
+    public void supprimerClient(Long id) {
+        Client client = trouverClientParId(id);
+        clientRepository.delete(client);
+    }
+}
